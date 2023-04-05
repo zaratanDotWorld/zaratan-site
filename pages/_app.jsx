68190458984e6c8998a4e088e781ff9fb4747083
Daniel Kronovet <kronovet@gmail.com>
2023-04-04 22:20:10 -0400

    Add mailchimp signup form

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import Layout from '../components/layout';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/global.css';
+import '../styles/mailchimp.css';
 
 export  default function Website({ Component, pageProps }) {
   return (
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,34 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 import zaratanPic from "../public/images/zaratan-art.jpg";
 
+function SignupForm () {
+  return (
+    <div id="mc_embed_signup">
+      <form
+        action="https://github.us18.list-manage.com/subscribe/post?u=7176b8b238db3ff30396bf211&amp;id=f82230a866"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        className="validate"
+        target="_blank"
+        noValidate
+      >
+        <div id="mc_embed_signup_scroll">
+          <input type="email" defaultValue="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="~ email address ~" required></input>
+          <br></br>
+          <br></br>
+          {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+          <div style={{position: "absolute", left: "-5000px"}} aria-hidden="true">
+            <input type="text" name="b_7176b8b238db3ff30396bf211_f82230a866" tabIndex="-1" defaultValue=""></input>
+          </div>
+          <div className="clear">
+            <input type="submit" value="Join the waitlist" name="subscribe" id="mc-embedded-subscribe" className="button"></input>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 export default function () {
   return (
@@ -13,6 +41,8 @@ export default function () {
           <h1 className="center spaced logo" style={{ fontSize: "92px" }}>Zaratan</h1>
           <p className="center spaced rainbow-text-animated" style={{ fontSize: "32px" }}>Find Your Place</p>
           <h1 className="center" style={{ fontSize: "92px" }}>&#x1F422;</h1>
+          <br></br>
+          <SignupForm></SignupForm>
         </Col>
         <Col className="col-lg-4 order-2 order-lg-1 p-4 p-sm-5 p-lg-4 p-xl-5 red">
           <h5>Zaratan is an emerging coliving company in Los Angeles, with a focus on freedom and accountability.</h5>
