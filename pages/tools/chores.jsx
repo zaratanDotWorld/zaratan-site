2227e71ec67ccdca6807b0afa94e214cf7ceacd3
Daniel Kronovet <kronovet@gmail.com>
2023-10-21 08:13:50 -0400

    First pass at app pages

--- pages/tools/chores.jsx
+++ pages/tools/chores.jsx
@@ -1,11 +1,27 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { Container, Row, Col, Carousel } from 'react-bootstrap';
 
 import Headpiece from '../../components/headpiece';
+import SlackButton from '../../components/slack';
 
-export default function () {
+import { getImages } from '../../utils/s3';
+
+export async function getStaticProps() {
+  const regex = /public\/images\/mirror\/chores.*\.jpg/i;
+  const images = await getImages({ regex });
+  return { props: { images } }
+}
+
+export default function ({ images }) {
+  const installUrl = "https://slack.com/oauth/v2/authorize?client_id=1003435112487.1005562872163" +
+    "&scope=channels:history,channels:join,channels:read,chat:write,commands,users:read&user_scope=";
+  const manualUrl = "https://github.com/zaratanDotWorld/mirror/wiki/Chores";
+
   return (
     <Container fluid>
-      <Row className="p-5">
+      <Row className="p-5 center">
        <Col />
         <Col>
           <Headpiece
@@ -14,6 +30,8 @@ export default function () {
               icon="&#x1f9e4;"
               scale={25}
             />
+            <br></br>
+            <SlackButton text="Install Now" url={installUrl} />
         </Col>
         <Col />
       </Row>
@@ -22,6 +40,16 @@ export default function () {
         <Col />
         <Col md={10} xl={6}>
           <h5><b>Chores is Zaratan's crown jewel</b>, making dirty dishes and passive-aggressive sticky notes a thing of the past.</h5>
+          <p>
+            Chores is a chore wheel for the 21st century, ensuring everyone does their part, on their terms.
+            Instead of restrictive chore schedules or ineffective "bragging boards," chores are worth points.
+            Everyone owes 100 points per month, and the longer a chore goes undone, the more points it's worth.
+          </p>
+          <p>
+            Folks co-create their list of chores, and they fine-tune the priorities so more points go to more important chores.
+            The Chores system is dynamic and flexible, letting people gravitate towards the work they like best.
+            The best part? It all happens like magic.
+          </p>
           <hr></hr>
         </Col>
         <Col />
@@ -30,7 +58,7 @@ export default function () {
       <Row className="green pb-5 px-5">
         <Col/>
         <Col md={5} xl={3}>
-          <h5>Old Way</h5>
+          <h5>&#x1f62d; Old Way</h5>
           <ul>
             <li>Dirty dishes</li>
             <li>Passive-aggressive sticky notes</li>
@@ -39,12 +67,12 @@ export default function () {
           </ul>
         </Col>
         <Col md={5} xl={3}>
-          <h5>New Way</h5>
+          <h5>&#x1f929; New Way</h5>
           <ul>
             <li>Clean sinks</li>
             <li>Communication and transparency</li>
             <li>Feeling empowered and appreciated</li>
-            <li>Tremendous flexibility</li>
+            <li>Endless flexibility</li>
           </ul>
         </Col>
         <Col/>
@@ -52,10 +80,51 @@ export default function () {
 
       <Row className="p-5">
         <Col />
-        <Col>
-          Coming soon
+        <Col lg={5} xl={4} className="p-3">
+          <Carousel interval={3600} variant="dark">
+              {
+                images.map((imageUri, i) =>
+                  <Carousel.Item key={i}>
+                    <Image
+                      className="rounded border border-3"
+                      src={imageUri}
+                      alt={imageUri}
+                      width={316}
+                      height={560}
+                      style={{ margin: "auto", display: "block" }}
+                    />
+                  </Carousel.Item>
+                )
+              }
+            </Carousel>
+        </Col>
+        <Col lg={5} xl={4} className="p-3">
+          <Carousel interval={3600} variant="dark">
+              {
+                images.reverse().map((imageUri, i) =>
+                  <Carousel.Item key={i}>
+                    <Image
+                      className="rounded border border-3"
+                      src={imageUri}
+                      alt={imageUri}
+                      width={316}
+                      height={560}
+                      style={{ margin: "auto", display: "block" }}
+                    />
+                  </Carousel.Item>
+                )
+              }
+            </Carousel>
         </Col>
         <Col />
+      </Row>
+
+      <Row className="pb-5 center">
+        <Col>
+          <p style={{ fontSize: "30px" }}>
+            &#x1f449; <Link href={manualUrl}>Read the manual</Link> &#x1f448;
+          </p>
+        </Col>
       </Row>
     </Container>
   )
--- pages/tools/things.jsx
+++ pages/tools/things.jsx
@@ -1,30 +1,122 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import Link from 'next/link';
+import Image from 'next/image';
 
-export default function () {
+import { Container, Row, Col, Carousel } from 'react-bootstrap';
+
+import Headpiece from '../../components/headpiece';
+import SlackButton from '../../components/slack';
+
+import { getImages } from '../../utils/s3';
+
+export async function getStaticProps() {
+  const regex = /public\/images\/mirror\/things.*\.jpg/i;
+  const images = await getImages({ regex });
+  return { props: { images } }
+}
+
+export default function ({ images }) {
+  const installUrl = "https://slack.com/oauth/v2/authorize?client_id=1003435112487.4076729845777" +
+    "&scope=channels:history,channels:read,chat:write,commands,reactions:write,users:read,channels:join&user_scope=";
+  const manualUrl = "https://github.com/zaratanDotWorld/mirror/wiki/Things";
+
   return (
     <Container fluid>
-      <Row className="p-5">
+      <Row className="p-5 center">
        <Col />
         <Col>
-          Coming soon
+          <Headpiece
+              mainText="Things"
+              subText="Keeping It Stocked"
+              icon="&#x1f6d2;"
+              scale={25}
+            />
+            <br></br>
+            <SlackButton text="Install Now" url={installUrl} />
         </Col>
         <Col />
       </Row>
 
-      <Row className="blue p-5">
+      <Row className="blue pt-5 px-5">
         <Col />
-        <Col md={8} xl={6}>
-          <b>Things</b>
+        <Col md={10} xl={6}>
+          <h5><b>Things is Zaratan's purchasing tool</b>, helping people easily manage shared funds.</h5>
+          <p>
+            Things lets folks co-create a list of shared staples and makes it easy to buy more when you start running low.
+            Proposed "buys" are approved by emoji-vote, with more upvotes needed to approve larger purchases.
+          </p>
+          <hr></hr>
         </Col>
         <Col />
+      </Row>
+
+      <Row className="blue pb-5 px-5">
+        <Col/>
+        <Col md={5} xl={3}>
+          <h5>&#x1f62d; Old Way</h5>
+          <ul>
+            <li>Many spreadsheets</li>
+            <li>Tracking inventory</li>
+            <li>Complex preferences</li>
+          </ul>
+        </Col>
+        <Col md={5} xl={3}>
+          <h5>&#x1f929; New Way</h5>
+          <ul>
+            <li>Simple process</li>
+            <li>Everyone can participate</li>
+            <li>Clear needs</li>
+          </ul>
+        </Col>
+        <Col/>
       </Row>
 
       <Row className="p-5">
         <Col />
-        <Col>
-          Coming soon
+        <Col lg={5} xl={4} className="p-3">
+          <Carousel interval={3600} variant="dark">
+              {
+                images.map((imageUri, i) =>
+                  <Carousel.Item key={i}>
+                    <Image
+                      className="rounded border border-3"
+                      src={imageUri}
+                      alt={imageUri}
+                      width={316}
+                      height={560}
+                      style={{ margin: "auto", display: "block" }}
+                    />
+                  </Carousel.Item>
+                )
+              }
+            </Carousel>
+        </Col>
+        <Col lg={5} xl={4} className="p-3">
+          <Carousel interval={3600} variant="dark">
+              {
+                images.reverse().map((imageUri, i) =>
+                  <Carousel.Item key={i}>
+                    <Image
+                      className="rounded border border-3"
+                      src={imageUri}
+                      alt={imageUri}
+                      width={316}
+                      height={560}
+                      style={{ margin: "auto", display: "block" }}
+                    />
+                  </Carousel.Item>
+                )
+              }
+            </Carousel>
         </Col>
         <Col />
+      </Row>
+
+      <Row className="pb-5 center">
+        <Col>
+          <p style={{ fontSize: "30px" }}>
+            &#x1f449; <Link href={manualUrl}>Read the manual</Link> &#x1f448;
+          </p>
+        </Col>
       </Row>
     </Container>
   )
